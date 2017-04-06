59c5daa6f84dfebfc65d80f941c632963af07e01
MoXiGeTuFei <minyz16@mails.tsinghua.edu.cn>
2017-04-06 09:52:46 +0800

    2017-4-1

--- utils/convert.py
+++ utils/convert.py
@@ -1,6 +1,16 @@
 import scipy.io
-import matplotlib.pyplot as plt
 import numpy as np
-matfile='../data/library.mat'
-data=scipy.io.loadmat(file_name=matfile)
-data.viewkeys()
+mymat='../data/mylib.mat'
+mydict={}
+mydict['freq']=np.loadtxt('../data/freq.txt')
+mydict['logStart']=np.loadtxt('../data/logStart.txt')
+mydict['logT']=np.loadtxt('../data/logT.txt')
+mydict['mixprob']=np.loadtxt('../data/mixprob.txt')
+mydict['mu']=np.loadtxt('../data/mu.txt')
+mydict['pkappa']=np.loadtxt('../data/pkappa.txt')
+mydict['pmat_nsub']=np.loadtxt('../data/pmat_nsub.txt')
+mydict['sigma']=np.loadtxt('../data/sigma.txt')
+mydict['theta']=np.loadtxt('../data/theta.txt')
+mydict['vsd']=np.loadtxt('../data/vsd.txt')
+scipy.io.savemat(mymat,mydict)
+
